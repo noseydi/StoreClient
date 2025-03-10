af9bab572bed603553b3d79c23fb81c418eef6df
nura <nura@DESKTOP-8HSSN9E>
2025-03-10 22:18:48 +0330

    6-6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../Environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit
+{
+  private backendUrl = environment.backendUrl;
+  constructor (private http: HttpClient)
+  {}
+ngOnInit():void
+{
+this.http.get<any>(this.backendUrl + '/products').subscribe((res)=>{
+  console.log(res);
+});
+}
   title = 'StoreClient';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +14,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration(withEventReplay())
